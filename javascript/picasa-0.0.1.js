92b1ccfc12aced064e68e8337c4165c8887c8904
bnson <bnson@live.com>
2016-12-29 23:52:12 +0700

    conntet

--- javascript/picasa-0.0.1.js
+++ javascript/picasa-0.0.1.js
@@ -29,7 +29,8 @@ function loadRecentPost_001(userid, albumid, authkey, thumbsize, photosize, marg
 
 	var scripts = document.getElementsByTagName('script');
 	var me = scripts[scripts.length-1];
-	var idDiv = "recentPosts";	
+	//var idDiv = "recentPosts";	
+	var idDiv = me.parentNode;	
 	//console.log('parent id', me.parentNode.id);	
 		
 	// Originally based on code from http://www.bloggingtips.com/2009/03/23/picasa-widgets-and-plugins-for-your-blog/
@@ -39,8 +40,8 @@ function loadRecentPost_001(userid, albumid, authkey, thumbsize, photosize, marg
 			"http://picasaweb.google.com/data/feed/api/user/" + userid + "/album/" + albumid + "?authkey=" + authkey + "&kind=photo&alt=json-in-script&callback=?",
 			function(data, status) {
 
-				$j("#" + idDiv).append("<div class='col-xs-6 col-md-3' id=\"" + albumid + "\"></div>");
-				$j("#" + albumid).append("<div class='row' id=\"picasaThumb_" + albumid + "\"></div>");								
+				//$j("#" + idDiv).append("<div class='col-xs-6 col-md-3' id=\"" + albumid + "\"></div>");
+				$j("#" + idDiv).append("<div class='row' id=\"picasaThumb_" + albumid + "\"></div>");								
 
 				var tmpCount = true;
 				$j.each(data.feed.entry, function(i, pic) {
